853f841e532edf40f380658dc51e23a9d9ad3cf1
hjdalton <hjdalton@hotmail.co.uk>
2021-07-20 14:31:07 +0100

    Heather Test

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,6 +1,6 @@
 require 'docking_station'
 describe DockingStation do
-  it { is_expected.to respond_to :release_bike}
+  it { is_expected.to respond_to :release_bike} #heather test
   
   it 'releases working bikes' do
     bike = subject.release_bike # bike = new element(method - release bike)
