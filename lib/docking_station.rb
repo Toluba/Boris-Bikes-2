e9431c9e8d00a911b62901744a8cc6068a479238
toluba <tomikeolubamowo@aol.com>
2021-07-21 16:08:48 +0100

    Still on chapter 12 but down to one failure

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,19 +1,22 @@
 class DockingStation
+  attr_reader :bike
   def release_bike
-    raise 'No bikes available' #Both raise and fail don't work (runtimeError)
-    unless @bike 
-      @bike
-    end
+    fail 'No bikes available' unless @bike
+    @bike
+    # raise 'No bikes available' #Both raise and fail don't work (runtimeError)
+    # unless @bike 
+    #   @bike
+    # end
   end
 
-  attr_reader :bike
   def docking_bikes(bike)
     @bike = bike
   end
-end
+
+end 
 
 class Bike 
   def working?
-
+    true
   end
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,28 +1,37 @@
 require 'docking_station'
 describe DockingStation do
-  it { is_expected.to respond_to :release_bike} #responds to release bike
+  it { is_expected.to respond_to :release_bike } #responds to release bike
   
   it 'releases working bikes' do
     bike = subject.release_bike # bike = new element(method - release bike)
-    expect(bike).to respond_to(:working?) # checking if be_working function shows that the bike is working
-    
+    expect(bike).to be_working
+    # (:working?) # checking if be_working function shows that the bike is working
   end
 
-  # it 'raises an error when there are no bikes available' do
-  #   expect {subject.release_bike}.to raise_error("No bikes available")
+  # it 'docking a bike' do 
+  #   bike = Bike.new 
+  #   expect(subject.docking_bikes(bike)).to eq bike #checking that docking bikes arguement is equal to an instance of the bike class
   # end
 
+  it {is_expected.to respond_to(:docking_bikes).with(1).argument } #checking that theres 1 arguement that responds to docking_bikes
 
-  it 'docking a bike' do 
-    bike = Bike.new #
-    expect(subject.docking_bikes(bike)).to eq bike #checking that docking bikes arguement is equal to an instance of the bike class
-  end
+  it { is_expected.to respond_to(:bike) }
 
-  it { is_expected.to respond_to(:docking_bikes).with(1).argument } #checking that theres 1 arguement that responds to docking_bikes
+  it 'returns docked bikes' do
+    bike = Bike.new
+    subject.docking_bikes(bike)
+    expect(subject.bike).to eq bike
+  end 
 
   describe '#release_bike' do
-    it 'raises an error when there are no bikes available' do 
-      expect {subject.release_bike}.to raise_error("No bikes available")
+    it 'releases a bike' do
+      bike = Bike.new
+      subject.docking_bikes(bike)
+      expect(subject.release_bike).to eq bike
+    end
+
+    it 'raises an error when there are no bikes available' do
+      expect { subject.release_bike }.to raise_error 'No bikes available'
     end
   end
 end
